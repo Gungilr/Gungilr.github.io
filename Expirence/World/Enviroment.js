ba6b0188ba535277baf86b41e68d5ff9bdc1485b
Gungilr <68489659+Gungilr@users.noreply.github.com>
2023-09-11 15:47:36 -0700

    site

---
+++ Expirence/World/Enviroment.js
@@ -0,0 +1,77 @@
+import * as THREE from "three";
+import Expirence from "../Expirence";
+import GSAP from "gsap";
+import GUI from "lil-gui"
+
+export default class Enviroment{
+    constructor(){
+        this.expirence = new Expirence();
+        this.scene = this.expirence.scene;
+        this.resources = this.expirence.resources;
+        this.room = this.expirence.world.room.actualRoom;
+
+
+        this.setSunlight();     
+        //this.setGui(); 
+    }
+    
+
+    setSunlight() {
+        this.sunLight = new THREE.DirectionalLight("#ffffff", 3);
+        this.sunLight.castShadow = true;
+        this.sunLight.shadow.camera.far = 20;
+        this.sunLight.shadow.mapSize.set(2048, 2048);
+        this.sunLight.shadow.normalBias = 0.05;
+        // const helper = new THREE.CameraHelper(this.sunLight.shadow.camera);
+        // this.scene.add(helper);
+
+        this.sunLight.position.set(-1.5, 7, 3);
+        this.scene.add(this.sunLight);
+
+        this.ambientLight = new THREE.AmbientLight("#ffffff", 1);
+        this.scene.add(this.ambientLight);
+    }
+
+    switchTheme(theme)
+    {
+        if(theme === "dark")
+        {
+            GSAP.to(this.sunLight.color, {
+                r: 0.25882352941176473, 
+                g: 0.16862745098039217, 
+                b: 0.47058823529411764
+            });
+            GSAP.to(this.ambientLight.color, {
+                r: 0.25882352941176473, 
+                g: 0.16862745098039217, 
+                b: 0.47058823529411764
+            });
+            GSAP.to(this.sunLight, {intensity: 0.78});
+            GSAP.to(this.ambientLight, {intensity: 0.78});
+            
+        }else{
+            GSAP.to(this.sunLight.color, {
+                r: 255 / 255,
+                g: 255 / 255,
+                b: 255 / 255,
+            });
+            GSAP.to(this.ambientLight.color, {
+                r: 255 / 255,
+                g: 255 / 255,
+                b: 255 / 255,
+            });
+            GSAP.to(this.sunlight, {intensity: 3});
+            GSAP.to(this.ambientLight, {intensity: 1});
+        }
+    }
+
+    resize()
+    {
+
+    }
+
+    update()
+    {
+        
+    }
+}
